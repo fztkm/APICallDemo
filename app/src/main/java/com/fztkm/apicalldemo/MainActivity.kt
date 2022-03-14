86561ecf735ea24486a047fc0552ba6f69e2b691
Takumi Fukuzawa <forthune.0517@gmail.com>
2022-03-14 21:09:02 +0900

    first commit

---
+++ app/src/main/java/com/fztkm/apicalldemo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fztkm.apicalldemo
+
+import android.app.Dialog
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
